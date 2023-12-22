fcf3b7c2faddef619284b3522e286b1a51defb09
Kayke CS <kaykecsgit@gmail.com>
2023-12-22 20:07:16 -0300

    Novas funções, novos botões adicionados, login, senha e cadastro

--- login.py
+++ login.py
@@ -1,57 +1,92 @@
-import customtkinter
-import tkinter
-from tkinter import RIGHT
-
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme('dark-blue')
-
-window = customtkinter.CTk()
-window.geometry('700x400')
-window.title('Sistema de login')
-window.iconbitmap('icon.ico')
-window.resizable(False, False)
-
-# tela
-img = tkinter.PhotoImage(file='log.png')
-label_img = customtkinter.CTkLabel(master=window, image=img)
-label_img.place(x=50, y=65)
+import customtkinter as ctk
+import tkinter as tk
+root = ctk.CTk()
 
 
-# frame
-frame = customtkinter.CTkFrame(master=window, width=350, height=396)
-frame.pack(side=(RIGHT))
+class Application():
+    def __init__(self):
+        self.root = ctk.CTk()
+        self.theme()
+        self.screen()
+        self.login()
+        self.root.mainloop()
+
+    def theme(self):
+        ctk.set_appearance_mode('dark')
+        ctk.set_default_color_theme('dark-blue')
+
+    def screen(self):
+        self.root.geometry('700x400')
+        self.root.title('Sistema de login')
+        self.root.iconbitmap('icon.ico')
+        self.root.resizable(False, False)
+
+    def login(self):
+        login_frame = ctk.CTkFrame(master=self.root, width=350, height=396)
+        login_frame.pack()
+
+        label = ctk.CTkLabel(master=login_frame,
+                             text='Sistema de login',
+                             font=('Roboto', 20))
+        label.place(x=25, y=5)
+        username_entry1 = ctk.CTkEntry(
+            master=login_frame,
+            placeholder_text='Nome de Usuário',
+            width=300,
+            font=("Arial", 10)
+        )
+        username_entry1.place(x=25, y=105)
+
+        username_label1 = ctk.CTkLabel(
+            master=login_frame,
+            text='*Esse campo exige caracter obrigatório.',
+            text_color='green'
+        )
+        username_label1.place(x=25, y=135)
+
+        password_entry2 = ctk.CTkEntry(
+            master=login_frame,
+            placeholder_text='Senha',
+            width=300,
+            font=("Arial", 10),
+            show='*'
+        )
+        password_entry2.place(x=25, y=175)
+
+        password_label2 = ctk.CTkLabel(
+            master=login_frame,
+            text='*Esse campo exige caracter obrigatório.',
+            text_color='green'
+        )
+        password_label2.place(x=25, y=205)
+
+        checkbox = ctk.CTkCheckBox(
+            master=login_frame,
+            text='Lembrar-se de mim sempre'
+        )
+        checkbox.place(x=25, y=235)
+
+        login_button = ctk.CTkButton(
+            master=login_frame,
+            text='Login',
+            width=300
+        )
+        login_button.place(x=25, y=285)
+
+        register_span = ctk.CTkLabel(
+            master=login_frame,
+            text='Não possui uma conta ?'
+        )
+        register_span.place(x=25, y=325)
+
+        register_button = ctk.CTkButton(
+            master=login_frame,
+            text='Cadastre-se',
+            width=150,
+            fg_color='green',
+            hover_color='#2D9334'
+        )
+        register_button.place(x=175, y=325)
 
 
-# frame widgets
-label = customtkinter.CTkLabel(master=frame, text='Login', font=(
-    'Roboto', 20, 'bold'), text_color=('white'))
-label.place(x=25, y=5)
-
-entry1 = customtkinter.CTkEntry(
-    master=frame,
-    placeholder_text='Nome de Usuário',
-    width=300, font=("Arial", 10)).place(x=25, y=105)
-
-label1 = customtkinter.CTkLabel(
-    master=frame,
-    text='*Esse campo exige caracter obrigatório.',
-    text_color='green').place(x=25, y=135)
-
-entry2 = customtkinter.CTkEntry(
-    master=frame,
-    placeholder_text='Senha',
-    width=300, font=("Arial", 10)).place(x=25, y=175)
-
-label2 = customtkinter.CTkLabel(
-    master=frame,
-    text='*Esse campo exige caracter obrigatório.',
-    text_color='green').place(x=25, y=205)
-
-checkbox = customtkinter.CTkCheckBox(
-    master=frame, text='Lembrar-se de mim sempre').place(x=25, y=235)
-
-button = customtkinter.CTkButton(
-    master=frame, text='LOGIN', width=300).place(x=25, y=285)
-
-
-window.mainloop()
+Application()
